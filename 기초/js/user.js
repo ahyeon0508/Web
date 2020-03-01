5e963f27015825106f6c7d01f294764c229f9887
ahyeon0508 <ahyeon0508@naver.com>
2020-03-01 23:37:09 +0900

    js practice

---
+++ 기초/js/user.js
@@ -0,0 +1,173 @@
+var user = [
+  {
+    "_id": "5e5bc5cb5795f055f7138280",
+    "age": 37,
+    "eyeColor": "blue",
+    "name": "Obrien Chase",
+    "gender": "male",
+    "email": "obrienchase@polarax.com",
+    "phone": "+1 (806) 444-3767"
+  },
+  {
+    "_id": "5e5bc5cbd6d003e4455a370e",
+    "age": 26,
+    "eyeColor": "blue",
+    "name": "Terry Watts",
+    "gender": "male",
+    "email": "terrywatts@polarax.com",
+    "phone": "+1 (955) 451-2605"
+  },
+  {
+    "_id": "5e5bc5cbb36ab78b2eb78696",
+    "age": 25,
+    "eyeColor": "blue",
+    "name": "Quinn Black",
+    "gender": "male",
+    "email": "quinnblack@polarax.com",
+    "phone": "+1 (809) 423-2022"
+  },
+  {
+    "_id": "5e5bc5cb06940f4601cc8261",
+    "age": 34,
+    "eyeColor": "green",
+    "name": "Angelica Guy",
+    "gender": "female",
+    "email": "angelicaguy@polarax.com",
+    "phone": "+1 (809) 424-2123"
+  },
+  {
+    "_id": "5e5bc5cb550403c686160d82",
+    "age": 38,
+    "eyeColor": "blue",
+    "name": "Wise Stone",
+    "gender": "male",
+    "email": "wisestone@polarax.com",
+    "phone": "+1 (852) 568-3177"
+  },
+  {
+    "_id": "5e5bc5cb9deab054eedc469a",
+    "age": 26,
+    "eyeColor": "green",
+    "name": "Poole Buchanan",
+    "gender": "male",
+    "email": "poolebuchanan@polarax.com",
+    "phone": "+1 (808) 458-3267"
+  },
+  {
+    "_id": "5e5bc5cbf0c7bd39fd53781f",
+    "age": 29,
+    "eyeColor": "green",
+    "name": "Snow Newman",
+    "gender": "male",
+    "email": "snownewman@polarax.com",
+    "phone": "+1 (850) 533-3398"
+  },
+  {
+    "_id": "5e5bc5cb2851eadcc2cd071f",
+    "age": 39,
+    "eyeColor": "green",
+    "name": "Margaret Haney",
+    "gender": "female",
+    "email": "margarethaney@polarax.com",
+    "phone": "+1 (898) 549-3755"
+  },
+  {
+    "_id": "5e5bc5cba1057e6b10d7ba67",
+    "age": 32,
+    "eyeColor": "blue",
+    "name": "West Michael",
+    "gender": "male",
+    "email": "westmichael@polarax.com",
+    "phone": "+1 (975) 504-3352"
+  },
+  {
+    "_id": "5e5bc5cb9fef29299988ddfc",
+    "age": 20,
+    "eyeColor": "brown",
+    "name": "Randall Bowman",
+    "gender": "male",
+    "email": "randallbowman@polarax.com",
+    "phone": "+1 (924) 417-3697"
+  },
+  {
+    "_id": "5e5bc5cbc2e2ceb7cfa64278",
+    "age": 20,
+    "eyeColor": "brown",
+    "name": "Kinney Buckner",
+    "gender": "male",
+    "email": "kinneybuckner@polarax.com",
+    "phone": "+1 (863) 471-2749"
+  },
+  {
+    "_id": "5e5bc5cb6f08eaa76774a91c",
+    "age": 40,
+    "eyeColor": "blue",
+    "name": "Cohen Guzman",
+    "gender": "male",
+    "email": "cohenguzman@polarax.com",
+    "phone": "+1 (888) 455-2300"
+  },
+  {
+    "_id": "5e5bc5cb1820f1464a5ace3d",
+    "age": 22,
+    "eyeColor": "brown",
+    "name": "Carver Young",
+    "gender": "male",
+    "email": "carveryoung@polarax.com",
+    "phone": "+1 (938) 508-3711"
+  },
+  {
+    "_id": "5e5bc5cb15496c753d5c4adb",
+    "age": 29,
+    "eyeColor": "brown",
+    "name": "Gloria Marquez",
+    "gender": "female",
+    "email": "gloriamarquez@polarax.com",
+    "phone": "+1 (835) 418-2104"
+  },
+  {
+    "_id": "5e5bc5cb70c7fbe0a4a2f16a",
+    "age": 30,
+    "eyeColor": "blue",
+    "name": "Leola Petty",
+    "gender": "female",
+    "email": "leolapetty@polarax.com",
+    "phone": "+1 (825) 471-3281"
+  },
+  {
+    "_id": "5e5bc5cba495d81e931aec45",
+    "age": 32,
+    "eyeColor": "brown",
+    "name": "Shawn Becker",
+    "gender": "female",
+    "email": "shawnbecker@polarax.com",
+    "phone": "+1 (917) 529-3094"
+  },
+  {
+    "_id": "5e5bc5cbe1a4800f4f60748f",
+    "age": 27,
+    "eyeColor": "green",
+    "name": "Marietta Keller",
+    "gender": "female",
+    "email": "mariettakeller@polarax.com",
+    "phone": "+1 (949) 419-3724"
+  },
+  {
+    "_id": "5e5bc5cb154742b6f186dcd7",
+    "age": 39,
+    "eyeColor": "blue",
+    "name": "Cervantes Ross",
+    "gender": "male",
+    "email": "cervantesross@polarax.com",
+    "phone": "+1 (892) 515-3294"
+  },
+  {
+    "_id": "5e5bc5cb5a0aea9b4729c035",
+    "age": 26,
+    "eyeColor": "blue",
+    "name": "Crosby Casey",
+    "gender": "male",
+    "email": "crosbycasey@polarax.com",
+    "phone": "+1 (875) 448-2551"
+  }
+]
