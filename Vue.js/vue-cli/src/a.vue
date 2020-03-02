82ccd67b5fdb73092dbf9fe7612ba10bc578beae
ahyeon0508 <ahyeon0508@naver.com>
2020-03-03 01:45:05 +0900

    Vue practice

---
+++ Vue.js/vue-cli/src/a.vue
@@ -0,0 +1,19 @@
+<template>
+    <!-- html -->
+    <div>header</div>
+</template>
+
+<script>
+export default {
+    // Javascript - 인스턴스 옵션
+    methods: {
+        addNum: function(){
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
