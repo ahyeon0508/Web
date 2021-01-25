580728f97b692960e2f55d8aa319c61ef5c013b8
ahyeon0508 <ahyeon_0508@naver.com>
2021-01-25 18:05:17 +0900

    에러 해결 및 풋터

--- covid-19/src/covid-19.vue
+++ covid-19/src/covid-19.vue
@@ -31,52 +31,52 @@
     <div class="shortcut-header">
       <b-navbar type="dark" variant="info" id="navbar-header">
         <b-container fluid="xl">
-          <b-row>
-            <b-col md="auto">
-              <b-button id="button-1" variant="info">바로알기</b-button>
-              <b-button id="button-2" variant="info">발생동향</b-button>
-              <b-button id="button-3" variant="info">뉴스&이슈</b-button>
-              <b-button id="button-4" variant="info">홍보자료&FAQ</b-button>
-              <b-button id="button-5" variant="info">피해지원정책</b-button>
-              <b-button id="button-6" variant="info">공지사항</b-button>
-              <b-button id="button-7" variant="info">사회적거리두기</b-button>
-              <b-button id="button-8" variant="info">마스크착용</b-button>
-            </b-col>
-            <b-col col>
-              <b-collapse id="nav-collapse" is-nav>
-                <b-navbar-nav class="ml-auto">
-                  <b-nav-form>
-                    <b-form-input placeholder="검색어를 입력하세요."></b-form-input>
-                    <!-- <b-button type="submit">Search</b-button> -->
-                  </b-nav-form>
-                </b-navbar-nav>
-              </b-collapse>
-            </b-col>
-          </b-row>
-          <b-tooltip target="button-1" placement="bottom">
-            <a href="http://ncov.mohw.go.kr/baroView.do?brdId=4&brdGubun=41">코로나19란?</a>
-          </b-tooltip>
-          <b-tooltip target="button-2" placement="bottom">
-            <a href="http://ncov.mohw.go.kr/bdBoardList_Real.do?brdId=1&brdGubun=11&ncvContSeq=&contSeq=&board_id=&gubun=">국내 발생 현황</a>
-          </b-tooltip>
-          <b-tooltip target="button-3" placement="bottom">
-            <a href="http://ncov.mohw.go.kr/tcmBoardList.do?brdId=3&brdGubun=">보도자료</a>
-          </b-tooltip>
-          <b-tooltip target="button-4" placement="bottom">
-            <a href="http://ncov.mohw.go.kr/infoBoardList.do?brdId=3&brdGubun=32">홍보자료</a>
-          </b-tooltip>
-          <b-tooltip target="button-5" placement="bottom">
-            <a href="http://ncov.mohw.go.kr/supportPolicyBoardList.do?brdId=5&brdGubun=51">일반인(대국민)</a>
-          </b-tooltip>
-          <b-tooltip target="button-6" placement="bottom">
-            <a href="http://ncov.mohw.go.kr/duBoardList.do?brdId=2&brdGubun=21">일반인</a>
-          </b-tooltip>
-          <b-tooltip target="button-7" placement="bottom">
-            <a href="http://ncov.mohw.go.kr/socdisBoardView.do?brdId=6&brdGubun=1">거리 두기 체계 개요</a>
-          </b-tooltip>
-          <b-tooltip target="button-8" placement="bottom">
-            <a href="http://ncov.mohw.go.kr/maskDutyBoardList.do?brdId=7&brdGubun=71">지침, 제도</a>
-          </b-tooltip>
+            <b-row>
+              <b-col md="auto">
+                <b-button id="button-1" variant="info">바로알기</b-button>
+                <b-button id="button-2" variant="info">발생동향</b-button>
+                <b-button id="button-3" variant="info">뉴스&이슈</b-button>
+                <b-button id="button-4" variant="info">홍보자료&FAQ</b-button>
+                <b-button id="button-5" variant="info">피해지원정책</b-button>
+                <b-button id="button-6" variant="info">공지사항</b-button>
+                <b-button id="button-7" variant="info">사회적거리두기</b-button>
+                <b-button id="button-8" variant="info">마스크착용</b-button>
+              </b-col>
+              <b-col col>
+                <b-collapse id="nav-collapse" is-nav>
+                  <b-navbar-nav class="ml-auto">
+                    <b-nav-form>
+                      <b-form-input placeholder="검색어를 입력하세요."></b-form-input>
+                      <!-- <b-button type="submit">Search</b-button> -->
+                    </b-nav-form>
+                  </b-navbar-nav>
+                </b-collapse>
+              </b-col>
+            </b-row>
+            <b-tooltip target="button-1" placement="bottom">
+              <a href="http://ncov.mohw.go.kr/baroView.do?brdId=4&brdGubun=41">코로나19란?</a>
+            </b-tooltip>
+            <b-tooltip target="button-2" placement="bottom">
+              <a href="http://ncov.mohw.go.kr/bdBoardList_Real.do?brdId=1&brdGubun=11&ncvContSeq=&contSeq=&board_id=&gubun=">국내 발생 현황</a>
+            </b-tooltip>
+            <b-tooltip target="button-3" placement="bottom">
+              <a href="http://ncov.mohw.go.kr/tcmBoardList.do?brdId=3&brdGubun=">보도자료</a>
+            </b-tooltip>
+            <b-tooltip target="button-4" placement="bottom">
+              <a href="http://ncov.mohw.go.kr/infoBoardList.do?brdId=3&brdGubun=32">홍보자료</a>
+            </b-tooltip>
+            <b-tooltip target="button-5" placement="bottom">
+              <a href="http://ncov.mohw.go.kr/supportPolicyBoardList.do?brdId=5&brdGubun=51">일반인(대국민)</a>
+            </b-tooltip>
+            <b-tooltip target="button-6" placement="bottom">
+              <a href="http://ncov.mohw.go.kr/duBoardList.do?brdId=2&brdGubun=21">일반인</a>
+            </b-tooltip>
+            <b-tooltip target="button-7" placement="bottom">
+              <a href="http://ncov.mohw.go.kr/socdisBoardView.do?brdId=6&brdGubun=1">거리 두기 체계 개요</a>
+            </b-tooltip>
+            <b-tooltip target="button-8" placement="bottom">
+              <a href="http://ncov.mohw.go.kr/maskDutyBoardList.do?brdId=7&brdGubun=71">지침, 제도</a>
+            </b-tooltip>
         </b-container>
       </b-navbar>
     </div>
@@ -88,99 +88,75 @@
     </b-container>
 
     <b-container>
-      <div class="left-main">
-          <div class="announce">
-            <p><strong>환자 현황</strong>(1.25.00시 기준, '20.1.3. 이후 누계)</p>
-            <p>자세히</p>
+      <b-row>
+        <b-col cols="4">
+          <div class="left-main">
+            <div class="announce">
+              <p><strong>환자 현황</strong>(1.25.00시 기준, '20.1.3. 이후 누계)</p>
+              <p>자세히</p>
+            </div>
+
+            <div class="today">
+              
+            </div>
+
+            <div class="total">
+
+            </div>
+
+            <div class="total-chart">
+
+            </div>
+
+            <div class="today-chart">
+
+            </div>
+
+            <div class="card-ad">
+              <b-carousel
+                id="carousel-1"
+                v-model="slide"
+                :interval="7000"
+                controls
+                indicators
+                @sliding-start="onSlideStart"
+                @sliding-end="onSlideEnd"
+              >
+
+                <b-carousel-slide img-src="http://ncov.mohw.go.kr/upload/ncov/20210122174150.png"></b-carousel-slide>
+                <b-carousel-slide img-src="http://ncov.mohw.go.kr/upload/ncov/20201021193932.png"></b-carousel-slide>
+                <b-carousel-slide img-src="http://ncov.mohw.go.kr/upload/ncov/20201015154559.jpg"></b-carousel-slide>
+
+              </b-carousel>
+            </div>
           </div>
-
-          <div class="today">
-            
-          </div>
-
-          <div class="total">
+        </b-col>
+        <b-col cols="8">
+          <div class="right-main">
 
           </div>
+        </b-col>
 
-          <div class="total-chart">
-
-          </div>
-
-          <div class="today-chart">
-
-          </div>
-
-          <div class="card-ad">
-            <b-carousel
-              id="carousel-1"
-              v-model="slide"
-              :interval="4000"
-              controls
-              indicators
-              background="#ababab"
-              img-width="1024"
-              img-height="480"
-              style="text-shadow: 1px 1px 2px #333;"
-              @sliding-start="onSlideStart"
-              @sliding-end="onSlideEnd"
-            >
-              <!-- Text slides with image -->
-              <b-carousel-slide
-                caption="First slide"
-                text="Nulla vitae elit libero, a pharetra augue mollis interdum."
-                img-src="https://picsum.photos/1024/480/?image=52"
-              ></b-carousel-slide>
-
-              <!-- Slides with custom text -->
-              <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
-                <h1>Hello world!</h1>
-              </b-carousel-slide>
-
-              <!-- Slides with image only -->
-              <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=58"></b-carousel-slide>
-
-              <!-- Slides with img slot -->
-              <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
-              <b-carousel-slide>
-                <template #img>
-                  <img
-                    class="d-block img-fluid w-100"
-                    width="1024"
-                    height="480"
-                    src="https://picsum.photos/1024/480/?image=55"
-                    alt="image slot"
-                  >
-                </template>
-              </b-carousel-slide>
-
-              <!-- Slide with blank fluid image to maintain slide aspect ratio -->
-              <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image">
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eros felis, tincidunt
-                  a tincidunt eget, convallis vel est. Ut pellentesque ut lacus vel interdum.
-                </p>
-              </b-carousel-slide>
-            </b-carousel>
-
-
-            <p class="mt-4">
-              Slide #: {{ slide }}<br>
-              Sliding: {{ sliding }}
-            </p>
-          </div>
-      </div>
-
-      <div class="right-main">
-
-      </div>
+      </b-row>
+      
     </b-container>
 
 
-    <b-container>
-      <div class="footer">
-
-      </div>
-    </b-container>
+    <div class="footer">
+      <b-container>
+        <a>질병관리청 관련기관 바로가기</a>&nbsp;&nbsp;
+        <a>보건환경연구원 바로가기</a>&nbsp;&nbsp;
+        <a>관련기관 바로가기</a>
+      </b-container>
+      <hr>
+      <b-container>
+        <span><strong>신고상담</strong></span>
+        <span>보건소</span>
+        <span>질병관리청 콜센터 1339</span>
+        <span>지역번호+120</span>
+        <span>지역번호+114</span>
+      </b-container>
+    </div>
 
   </div>
 </template>
@@ -194,10 +170,14 @@ export default {
     }
   },
   methods: {
+    // eslint-disable-next-line no-unused-vars
     onSlideStart(slide) {
+      console.log(slide);
       this.sliding = true
     },
+    // eslint-disable-next-line no-unused-vars
     onSlideEnd(slide) {
+      console.log(slide);
       this.sliding = false
     }
   }
@@ -223,6 +203,11 @@ button{
 
 .ad-img{
   border: 1px solid gray;
+}
+
+.footer{
+  padding: 20px 0 20px 0;
+  background-color: #f8f8f8;
 }
 
 </style>
